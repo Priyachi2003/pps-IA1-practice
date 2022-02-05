2acd0bd6caa24b29bc003284a1c770f76a2b7ef8
Priyachi2003 <96510349+Priyachi2003@users.noreply.github.com>
2022-02-05 12:25:52 +0000

    p3original

---
+++ p3original.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int input_n()
+{
+  int n;
+  printf("enter the values to add\n");
+  scanf("%d",&n);
+  return n;
+}
+int sum_n(int n)
+{
+  int sum,i;
+  sum=0;
+  for(int i=1;i<n;i++)
+  {
+    sum=sum+i;
+  }
+  return sum;
+}
+void output(int n,int sum)
+{
+  int i;
+   for(int i=1;i<n;i++)
+   {
+     printf("%d+",i);
+   }
+   printf("%d is %d",i,sum);
+}
+int main()
+{
+  int n ,sum;
+  n=input_n();
+  sum=sum_n(n);
+  output(n,sum);
+  return sum;
+  
+}
